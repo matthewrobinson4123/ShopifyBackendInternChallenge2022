a6db934af0331d0552f86e9a96ecde9140f9a634
Matthew Robinson <mattthew.robinson4123@gmail.com>
2022-05-14 17:32:53 -0400

    initialize main.py, imported objects

---
+++ main.py
@@ -0,0 +1,26 @@
+############################
+# Author: Matthew Robinson
+# 
+# Shopify Backend Developer Intern Challenge 2022
+############################
+
+
+# import objects
+from inventoryItem import InventoryItem
+from inventorySite import InventorySite
+
+
+# sample items to test with
+inventory = [InventoryItem("Warhammer Space Marmines", 42), InventoryItem("Lego Star Destroyer", 5), InventoryItem("Settlers of Catan", 16)]
+
+# Inistialize system
+sites = [InventorySite("Geekdom")]
+for item in inventory:
+    sites[0].addToInventory(item)
+
+
+
+# Logistics program, allows user to create, edit and view Inventory sites and products
+def main():
+    print("Hello! Welcome to Geek imporium!")
+    print("")
