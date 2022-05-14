6e515c71bc0a074565d74496bfec6bd7f2f4358c
Matthew Robinson <mattthew.robinson4123@gmail.com>
2022-05-14 16:56:41 -0400

    InventoryItem and InventorySite finished

--- inventoryItem.py
+++ inventoryItem.py
@@ -1,8 +1,28 @@
-
+# This class creates InventoryItem objects, assigning them names and quantities  
 
 class InventoryItem:
-    def __init__(self, name, amount):
-        self._name = name
-        self._amount = amount
 
-    
+    # Constructor for InventoryItem objects
+    def __init__(self, name, quantity):
+        self._name = name
+        self._quantity = quantity
+
+
+    # Returns the name of the item
+    def name(self):
+        return self._name
+
+
+    # Sets the name for the item
+    def setName(self, newName):
+        self._name = newName
+
+
+    # Returns how many of this object exist
+    def quantity(self):
+        return self._quantity
+
+
+    # Sets a new quantity of items
+    def adjustQuantity(self, newQuantity):
+        self._quantity = newQuantity
--- inventorySite.py
+++ inventorySite.py
@@ -1,8 +1,36 @@
-
+# This class creates InventorySite objects, assigning them names and an inventory list
 
 class InventorySite:
-    def __init__(self, name, inventoryList):
-        self._name = name
-        self._inventoryList = inventoryList
 
-    
+    # Constructor for InventorySite objects
+    def __init__(self, siteName):
+        self._name = siteName
+        self._inventoryList = []
+
+
+    # Returns the name of the inventory site
+    def siteName(self):
+        return self._name
+
+
+    # Sets the name of the inventory site
+    def setSiteName(self, newName):
+        self._name = newName
+
+
+    # Adds item to the site inventory
+    def addToInventory(self, item):
+        self._inventoryList.append(item) # adds item to inventory list
+        self._inventoryList.sort() # sorts list to alphabetic order
+
+
+    # Returns site inventory list
+    def getSiteInventory(self):
+        return self._inventoryList
+
+
+    # Prints every single item within the site's inventory list
+    def printSiteInventory(self):
+        print("Inventory for ", self.siteName)
+        for item in self._inventoryList:
+            print(item.name,"Quantity: " item.quantity "\n")
