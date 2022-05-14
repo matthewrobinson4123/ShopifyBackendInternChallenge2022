f4b8794c67e4e8280b8418ba442dfe68de1bdbfc
Matthew Robinson <mattthew.robinson4123@gmail.com>
2022-05-14 17:42:16 -0400

    added print statement menu

--- main.py
+++ main.py
@@ -23,4 +23,10 @@ for item in inventory:
 # Logistics program, allows user to create, edit and view Inventory sites and products
 def main():
     print("Hello! Welcome to Geek imporium!")
-    print("")
+    print("What are your logistical desires today? Please select from the follwing:")
+    print('CREATE; create an item or inventory site \
+            EDIT; edit item name or quantity or change site name \
+            DELETE; remove an item or site from system WARNING deleting a site deletes all of its inventory \
+            VIEW; view inventory for specified site or for all sites \
+            TRANSFER; transfer items from one site to another \
+            EXIT; exit from the system')
