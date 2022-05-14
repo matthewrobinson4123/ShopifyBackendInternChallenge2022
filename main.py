e5556f8decc5cad2a85bcee3daf77f5ab3611a5f
Matthew Robinson <mattthew.robinson4123@gmail.com>
2022-05-14 18:43:25 -0400

    handle user input from menu

--- main.py
+++ main.py
@@ -8,6 +8,7 @@
 # import objects
 from inventoryItem import InventoryItem
 from inventorySite import InventorySite
+from operations import handleInput
 
 
 # sample items to test with
@@ -22,14 +23,67 @@ for item in inventory:
 
 # Logistics program, allows user to create, edit and view Inventory sites and products
 def main():
+
+    # Greeting and options menu
     print("\nHello! Welcome to Geek Emporium!\n")
     print("What are your logistical desires today? Please select from the following: \n")
-    print("CREATE;      create an item or inventory site \n"
+
+    userInput = input("CREATE;      create an item or inventory site \n"
             "EDIT;      edit item name or quantity or change site name \n"
             "DELETE;        remove an item or site from system WARNING deleting a site deletes all of its inventory \n"
             "VIEW;      view inventory for specified site or for all sites \n"
             "TRANSFER;      transfer items from one site to another \n"
             "EXIT;      exit from the system\n")
 
+    while (userInput.lower() != "exit"):
+        handleInput(userInput.lower())
 
+        userInput = input("Please select from the following: \n\n"
+            "CREATE;      create an item or inventory site \n"
+            "EDIT;      edit item name or quantity or change site name \n"
+            "DELETE;        remove an item or site from system WARNING deleting a site deletes all of its inventory \n"
+            "VIEW;      view inventory for specified site or for all sites \n"
+            "TRANSFER;      transfer items from one site to another \n"
+            "EXIT;      exit from the system\n")
+
+    print("Thank you for using Geek Emporium, have a nerdy day!")
+
+
+
+
+def handleInput(input):
+    
+    if input == "create":
+        create()
+
+    elif input == "edit":
+        edit()
+
+    elif input == "delete":
+        delete()
+
+    elif input == "view":
+        view()
+
+    elif input == "transfer":
+        transfer()
+    
+    else:
+        print("Invalid selection, please try again. \n")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Runs code
 main()
