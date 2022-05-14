ddddebb9d62eb4174ffc7fa3e6e106fb415f9833
Matthew Robinson <mattthew.robinson4123@gmail.com>
2022-05-14 15:30:00 -0400

    initialize classes with protected variables

---
+++ inventoryItem.py
@@ -0,0 +1,8 @@
+
+
+class InventoryItem:
+    def __init__(self, name, amount):
+        self._name = name
+        self._amount = amount
+
+    
---
+++ inventorySite.py
@@ -0,0 +1,8 @@
+
+
+class InventorySite:
+    def __init__(self, name, inventoryList):
+        self._name = name
+        self._inventoryList = inventoryList
+
+    
